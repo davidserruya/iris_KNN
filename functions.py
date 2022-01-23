710e51663d84b59a955e09fa064e8eeb6ecbdcb7
David Serruya <david.serruya@etu.univ-paris1.fr>
2022-01-23 23:17:52 +0100

    new version

--- functions.py
+++ functions.py
@@ -1,23 +1,23 @@
-from ctypes import alignment
-import streamlit as st
-from PIL import Image
-import pandas
-import matplotlib.pyplot as plt
+from os import supports_effective_ids
 from sklearn.neighbors import KNeighborsClassifier
 
-def findk (d,lab):
+# Fonction qui permet de trouver le K optimal
+def findk (values,species):
   klist=[]
+  # tester K alant de 1 à 15
   for i in range (1,15):
     model = KNeighborsClassifier(n_neighbors=i)
-    model.fit(d,lab)
-    error = 1 - model.score(d, lab)
+    model.fit(values,species)
+    error = 1 - model.score(values,species)
     klist.append(error)
   min1=min(klist)
   k=klist.index(min1)+1
   return k
 
-def findPrediction(x,y,lab,longueur,largeur):
-  d=list(zip(x,y))
-  model = KNeighborsClassifier(n_neighbors=findk(d,lab))
-  model.fit(d,lab)
+# Fonction qui permet de définir d'après l'algorithme des k plus proches voisins 
+# l'espèce de l'iris demandé selon sa longueur et sa largeur de taille de pétale
+def findPrediction(x,y,species,longueur,largeur):
+  values=list(zip(x,y))
+  model = KNeighborsClassifier(n_neighbors=findk(values,species))
+  model.fit(values,species)
   return model.predict([[longueur,largeur]])
--- interface.py
+++ interface.py
@@ -7,26 +7,34 @@ from sklearn.neighbors import KNeighborsClassifier
 from functions import findk,findPrediction
 
 
-#traitement CSV
-iris=pandas.read_csv("/Users/david/Desktop/iris_KNN/iris.csv")
-x=iris.loc[:,"petal_length"]
-y=iris.loc[:,"petal_width"]
-lab=iris.loc[:,"species"]
-#fin traitement CSV
+# Traitement CSV
+iris=pandas.read_csv("/Users/david/Desktop/iris_KNN/iris.csv")                                                                                                  
+x=iris.loc[:,"petal_length"]                                                                                                                                    
+y=iris.loc[:,"petal_width"]                                                                                                                                     
+species=iris.loc[:,"species"]                                                                                                                                       
+# Fin traitement CSV
 
-
+# Affichage barre latérale
+st.sidebar.markdown("<h1 style='text-align: center; color: red;'>IRIS MENU</h1>", unsafe_allow_html=True)
 st.sidebar.title('Déterminer l\'espèce de votre iris : ')
 longueur = st.sidebar.slider('Entrez la longueur en cm', 0.0, 10.0, 0.1 )
 largeur= st.sidebar.slider('Entrez la largueur en cm', 0.0, 10.0, 0.1  )
+# Fin affichage barre latérale
 
+# Appel fonction de l'algorithme des K plus proches voisins
+prediction= findPrediction(x,y,species,longueur,largeur)
+# Fin appel
 
+# Affichage page principale
+st.markdown("<h1 style='text-align: center; color: red;'>IRIS INTERFACE</h1>", unsafe_allow_html=True)
 """
-En 1936, Edgar Anderson a collecté des données sur 3 espèces d\'iris : iris setosa, iris virginica et iris versicolor.
+En 1936, Edgar Anderson a collecté des données sur 3 espèces d\'iris : l\'iris setosa, l\'iris virginica et l\'iris versicolor.
 """
 image = Image.open('/Users/david/Desktop/iris_KNN/iris_setosa.jpeg')
 image2 = Image.open('/Users/david/Desktop/iris_KNN/iris_versicolor.jpeg')
 image3 = Image.open('/Users/david/Desktop/iris_KNN/iris_virginica.jpeg')
 st.image([image,image2,image3],caption=["SETOSA","VERSICOLOR","VIRGINICA"])
+
 """
 Pour chaque iris étudié, Anderson a mesuré (en cm) :
 * la largeur des sépales
@@ -36,35 +44,42 @@ Pour chaque iris étudié, Anderson a mesuré (en cm) :
 """
 st.write('Par souci de simplification, nous nous intéresserons uniquement à la largeur et à la longueur des pétales.')
 
-#algo knn
-prediction= findPrediction(x,y,lab,longueur,largeur)
-#fin algo knn
 
-#graphique
+# Affichage graphique des K plus proches voisins
 plt.axis('equal')
-plt.scatter(x[lab == 0], y[lab == 0], color='g', label='setosa')
-plt.scatter(x[lab == 1], y[lab == 1], color='r', label='versicolor')
-plt.scatter(x[lab == 2], y[lab == 2], color='b', label='virginica')
+plt.scatter(x[species == 0], y[species == 0], color='g', label='setosa')
+plt.scatter(x[species == 1], y[species == 1], color='r', label='versicolor')
+plt.scatter(x[species == 2], y[species == 2], color='b', label='virginica')
 plt.scatter(longueur, largeur, color='k')
 plt.legend()
 plt.savefig('saved_figure.png')
-st.image('saved_figure.png')
-#fin graphique
+#fin affichage graphique des K plus proches voisins
 
 
-#Affichage résultats
+# Affichage résultats
 st.write("Votre iris a des pétales de longueur de ", longueur, ' cm et de largeur de ',largeur,' cm.')
-st.write("D'après l'algorithme des k plus proches voisins, où ", findk(list(zip(x,y)),lab)," est le K optimal, votre iris est : ")
+image = Image.open('saved_figure.png')
+col1, col2, col3 = st.columns([0.2, 1, 0.2])
+col2.image(image, use_column_width=True)
+st.write("D'après l'algorithme des k plus proches voisins, où ", findk(list(zip(x,y)),species)," est le K optimal, votre iris est de l'espèce : ")
 
 if prediction[0]==0:
   image = Image.open('/Users/david/Desktop/iris_KNN/iris_setosa.jpeg')
-  st.image(image, caption='SETOSA')
-if prediction[0]==1:
+  col1, col2, col3 = st.columns([0.2, 0.4, 0.2])
+  col2.image(image, use_column_width=True, caption='SETOSA')
+elif prediction[0]==1:
   image = Image.open('/Users/david/Desktop/iris_KNN/iris_versicolor.jpeg')
-  st.image(image, caption='VERSICOLOR')
-if prediction[0]==2:
+  image = Image.open('/Users/david/Desktop/iris_KNN/iris_versicolor.jpeg')
+  col1, col2, col3 = st.columns([0.2, 0.4, 0.2])
+  col2.image(image, use_column_width=True, caption='VERSICOLOR')
+else:
   image = Image.open('/Users/david/Desktop/iris_KNN/iris_virginica.jpeg')
-  st.image(image, caption='VIRGINICA')
+  col1, col2, col3 = st.columns([0.2, 0.4, 0.2])
+  col2.image(image, use_column_width=True, caption='VIRGINICA')
+# Fin affichage résultats
+
+# Fin affichage page principale
+
 
 
 
