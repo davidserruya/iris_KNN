a0c5eb836b1fe53b38920cbd8790f68b78af06c6
davidserruya <56997072+davidserruya@users.noreply.github.com>
2022-02-20 23:51:45 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -38,7 +38,7 @@ def initialiseIris():
          kopt=n_neighbors
          model=clf
     target_pred=model.predict(data)
-    c=classification_report(target, target, target_names=['class 0', 'class 1', 'class 2'])
+    c=classification_report(target, target_pred, target_names=['class 0', 'class 1', 'class 2'])
     return model,x,y,target,kopt,accuracy,evals,c
 
 def initialiseDigit():
