9c1ce3041cac13aa47e531ef39a9cca35c2da03e
davidserruya <56997072+davidserruya@users.noreply.github.com>
2022-02-20 23:39:33 +0100

    Update interface_iris.py

--- interface_iris.py
+++ interface_iris.py
@@ -33,7 +33,7 @@ def initialise():
 # Fin traitement CSV
 model,x,y,species,kopt,c=initialise();
 
-st.dataframe(c)
+st.write(c)
 
 # Affichage page principale
 st.markdown("<h1 style='text-align: center; color: red;'>IRIS INTERFACE</h1>", unsafe_allow_html=True)
